b6aca361b1b4e8895f7f12054721b939d829baf2
Artem Telesh <decarea@yahoo.com>
2020-08-10 23:59:33 -0400

    configured docker upload script

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,14 +2,24 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
+set -e
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+local_name=flask_prediction
+docker_user=decisa
+dockerpath=$docker_user/$local_name
 
 # Step 2:  
+# log out current docker user and login the one we want to upload to
 # Authenticate & tag
+docker logout
+echo "please enter password for docker user: $docker_user :"
+docker login -u $docker_user
+docker tag $local_name $dockerpath
 echo "Docker ID and Image: $dockerpath"
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
