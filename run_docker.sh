35367968fe9202b2d3e78c98830f6c93ce0daf14
Artem Telesh <decarea@yahoo.com>
2020-08-10 22:27:17 -0400

    implemented dockerfile plus script to run

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t flask_prediction:latest .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+# docker run --name=flask_container -p 80:80 flask_prediction
+docker run -p 8000:80 flask_prediction
