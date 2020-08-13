7e846da93fc3a7d1a19fb211484747545e4b164f
Artem Telesh <decarea@yahoo.com>
2020-08-13 01:10:25 -0400

    removed unused imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, make_response, url_for
+from flask import Flask, request, render_template, redirect
 from flask.logging import create_logger
 import logging
 import json
